c9ed5932ef8f04be0887f84dd889cea53c352998
JulienQueant <julien.queant@u-psud.fr>
2020-05-04 14:25:32 +0200

    Step3

--- websocket-server.js
+++ websocket-server.js
@@ -5,11 +5,15 @@ const wss = new WebSocket.Server({ port: process.env.WS_PORT });
 wss.on('connection', ws => {
     console.log('Connection established');
 
-    ws.on('error', () => {
-        console.log('Error')
+    ws.on('message', message => {
+        console.log('Received message: ', message);
     });
 
     ws.on('close', () => {
         console.log('Connection closed by client');
     });
+});
+
+wss.on('error', error => {
+    console.log('Error', error)
 });
