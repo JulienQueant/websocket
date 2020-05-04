f1412094c99e815323a82e5b4de0daa7d92ddddf
JulienQueant <julien.queant@u-psud.fr>
2020-05-04 15:28:02 +0200

    Step4

--- websocket-server.js
+++ websocket-server.js
@@ -5,8 +5,14 @@ const wss = new WebSocket.Server({ port: process.env.WS_PORT });
 wss.on('connection', ws => {
     console.log('Connection established');
 
+    ws.send(JSON.stringify({
+        user: 'server',
+        message: 'Welcome!'
+    }));
+
     ws.on('message', message => {
         console.log('Received message: ', message);
+        ws.send(message)
     });
 
     ws.on('close', () => {
